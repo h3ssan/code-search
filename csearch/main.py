ab63b7bbe255cfe02746a6ba7b55e8f64d38e597
Hassan A. Hashim <h3ssan@protonmail.com>
2025-08-05 14:58:21 +0300

    refactor(main.py): Simplify caching logic and remove unused functions

--- csearch/main.py
+++ csearch/main.py
@@ -1,75 +1,36 @@
 from sys import argv
-from os import listdir, path
+from os import path
 
-from .file_type import get_file_type, get_file_ext, is_binary
 from .db import store_cached_files
-from .types import CachedFile
 from .config import Config
+from .caching import cache_repo
 
-
-def cache_file(cached_data: dict, file_path: str):
-    file_type = get_file_type(file_path)
-    file_extension = get_file_ext(file_path)
-    size_bytes = path.getsize(file_path)
-    file_path_relative = file_path.lstrip(Config.REPO_DIR + path.sep)
-
-    if Config.IGNORE_BINARY and is_binary(file_path):
-        return
-    
-    if Config.IGNORE_UNKNOWN_EXT and (file_extension == "unknown" or file_type == "unknown"):
-        return
-    
-    if Config.IGNORE_IMAGES and file_type == "image":
-        return
-    
-    if Config.IGNORE_EMPTY_FILES and size_bytes == 0:
-        return
-    
-    with open(file_path, 'r', encoding=Config.ENCODING, errors="replace") as file:
-        content = file.read()
-
-    cached_data[file_path_relative] = CachedFile(
-        file_type=file_type,
-        file_ext=file_extension,
-        size_bytes=size_bytes,
-        content=content
-    )
-
-
-def cache_repo(cached_data: dict, dir_path: str):
-    """
-    Caches repository data into a dictionary.
-    """
-    for item in listdir(dir_path):
-        if item.startswith('.') and Config.IGNORE_HIDDEN:
-            continue
-
-        item_path = path.join(dir_path, item)
-        if path.isdir(item_path) and item not in Config.IGNORE_DIRS:
-            cache_repo(cached_data, item_path)
-        else:
-            if item in Config.IGNORE_FILES:
-                print(f"Skipping ignored file: {item}")
-                continue
-            
-            cache_file(cached_data, item_path)
-
-def main():
+def set_repo_dir():
     Config.REPO_DIR = argv[1] if len(argv) > 1 else Config.REPO_DIR
     print(f"Using repository directory: {Config.REPO_DIR}")
+
+def validate_repo_dir():
     if not path.exists(Config.REPO_DIR):
         print(f"Repository directory does not exist: {Config.REPO_DIR}")
         exit(1)
 
+def cache_repository():
     print("Caching repository data...")
     cached_data = {}
-
     cache_repo(cached_data, Config.REPO_DIR)
+    print(f"Cached {len(cached_data):,} files.")
+    return cached_data
 
-    print(f"Cached {len(cached_data):,} files. Inserting into database...")
+def store_cache(cached_data):
+    print("Inserting into database...")
     store_cached_files(cached_data)
-    
     print("Repository data cached successfully.")
+
+def main():
+    set_repo_dir()
+    validate_repo_dir()
+    cached_data = cache_repository()
+    store_cache(cached_data)
 
 if __name__ == "__main__":
     main()
